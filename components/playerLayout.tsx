e229aa23ae54a404ace9d0822a7219018271b502
ousama <ousawack@gmail.com>
2023-02-23 00:22:26 +0100

    installed dependencies & fixed issues

---
+++ components/playerLayout.tsx
@@ -0,0 +1,13 @@
+import {Box} from '@chakra-ui/layout';
+import React from 'react';
+
+const playerLayout = ({children}) => {
+  return (
+    <Box>
+      Layout
+      {children}
+    </Box>
+  )
+}
+
+export default playerLayout;
