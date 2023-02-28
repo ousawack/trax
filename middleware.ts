87b316af8201466ac8bd1a5a31a1ea36fe5e3a72
ousama <ousawack@gmail.com>
2023-02-28 16:21:43 +0100

    latest version compatibility

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,18 @@
-import { NextResponse } from "next/server";
+import {NextRequest, NextResponse } from "next/server";
 
-const signedInPages = ["/", "/playlist", "/library"];
+export const config = {
+  matcher: ["/", "/playlist", "/library"]
+};
 
-export default function middleware(req) {
-  if (signedInPages.find((p) => p === req.nextUrl.pathname)) {
-    const token = req.cookies.TRAX_ACCESS_TOKEN;
-    const url = req.nextUrl.clone();
-    url.pathname = "/signin";
+export default function middleware(req: NextRequest) {
+    const token = req.cookies.get("TRAX_ACCESS_TOKEN");
+
+    console.log("middleware.ts");
+    console.log("Token:", token);
+
     if (!token) {
-      return NextResponse.rewrite(url);
+      const url = req.nextUrl.clone()
+      url.pathname = "/signin";
+      return NextResponse.redirect(url);
     }
-  }
 }
