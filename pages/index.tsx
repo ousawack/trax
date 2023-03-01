46aec449c3a97658b6898a91a038fdcdd737bf17
ousama <ousawack@gmail.com>
2023-03-01 01:54:27 +0100

    gradient background & ui layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,9 @@
+import GradientLayout from "../components/gradientLayout";
+
 const Home = () => {
-  return <div>home</div>;
+  return <GradientLayout color={"green"} image={"https://dl.dropboxusercontent.com/s/bgiv0ssz3xpotz9/peep.png?dl=0"} subtitle={"profile"} title={"Ousama Ajebbar"} description={"15 public playlists"} roundImage>
+    <div>home page</div>
+  </GradientLayout>
 };
 
 export default Home;
