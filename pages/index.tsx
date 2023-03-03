593ae88714108bc935ecdeb155f69eb6aa409fe5
ousama <ousawack@gmail.com>
2023-03-03 17:04:34 +0100

    updated artist ui layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import { Box, Flex, Text } from "@chakra-ui/layout";
+import { Image } from "@chakra-ui/react";
 import GradientLayout from "../components/gradientLayout";
 import prisma from "../lib/prisma";
 
 const Home = ({ artists }) => {
   return (
     <GradientLayout
-      color={"green"}
+      color={"purple"}
       image={
         "https://dl.dropboxusercontent.com/s/bgiv0ssz3xpotz9/peep.png?dl=0"
       }
@@ -14,20 +15,32 @@ const Home = ({ artists }) => {
       description={"15 public playlists"}
       roundImage
     >
-      <Box color={"white"}>
-        <Box>
-          <Text>
+      <Box color={"white"} paddingX="40px">
+        <Box marginBottom={"40px"}>
+          <Text fontSize={"2xl"} fontWeight="bold">
             Top artists this month
           </Text>
-          <Text>
-            Only visible to you
-          </Text>
+          <Text fontSize={"md"}>Only visible to you</Text>
         </Box>
         <Flex>
-          {artists.map(artist => (
-            <Text>
-              {artist.name}
-            </Text>
+          {artists.map((artist) => (
+            <Box paddingX={"10px"} width="20%">
+              <Box
+                bg={"gray.900"}
+                borderRadius="4px"
+                padding={"15px"}
+                width="100%"
+              >
+                <Image
+                  src="https://placekitten.com/300/300"
+                  borderRadius={"100%"}
+                />
+                <Box marginTop={"20px"}>
+                  <Text fontSize={"large"}>{artist.name}</Text>
+                  <Text fontSize={"small"}>Artist</Text>
+                </Box>
+              </Box>
+            </Box>
           ))}
         </Flex>
       </Box>
@@ -38,7 +51,7 @@ const Home = ({ artists }) => {
 export const getServerSideProps = async () => {
   const artists = await prisma.artist.findMany({});
 
-  console.log(artists)
+  console.log(artists);
 
   return {
     props: { artists },
