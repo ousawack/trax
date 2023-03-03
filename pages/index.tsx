b11b3a360c60f45022008fcfefca52cf15e5ffa0
ousama <ousawack@gmail.com>
2023-03-03 01:46:56 +0100

    server side data fetching

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,31 @@
 import GradientLayout from "../components/gradientLayout";
+import prisma from "../lib/prisma";
 
-const Home = () => {
-  return <GradientLayout color={"green"} image={"https://dl.dropboxusercontent.com/s/bgiv0ssz3xpotz9/peep.png?dl=0"} subtitle={"profile"} title={"Ousama Ajebbar"} description={"15 public playlists"} roundImage>
-    <div>home page</div>
-  </GradientLayout>
+const Home = ({ artists }) => {
+  return (
+    <GradientLayout
+      color={"green"}
+      image={
+        "https://dl.dropboxusercontent.com/s/bgiv0ssz3xpotz9/peep.png?dl=0"
+      }
+      subtitle={"profile"}
+      title={"Ousama Ajebbar"}
+      description={"15 public playlists"}
+      roundImage
+    >
+      <div>home page</div>
+    </GradientLayout>
+  );
+};
+
+export const getServerSideProps = async () => {
+  const artists = await prisma.artist.findMany({});
+
+  console.log(artists)
+
+  return {
+    props: { artists },
+  };
 };
 
 export default Home;
