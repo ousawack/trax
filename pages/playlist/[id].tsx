bb342a41f1d2db04c25108814ea422fc7b92055d
ousama <ousawack@gmail.com>
2023-03-08 18:02:51 +0100

    added random colors

--- pages/playlist/[id].tsx
+++ pages/playlist/[id].tsx
@@ -1,8 +1,32 @@
+import GradientLayout from "../../components/gradientLayout";
+import { SongsTable } from "../../components/songsTable";
 import { validateToken } from "../../lib/auth";
 import prisma from "../../lib/prisma";
 
+const getBGColor = (id) => {
+  const colors = [
+    "red",
+    "green",
+    "blue",
+    "orange",
+    "purple",
+    "gray",
+    "teal",
+    "yellow",
+    "indigo",
+  ];
+
+  return colors[id - 1] || colors[Math.floor(Math.random() * colors.length)];
+};
+
 const Playlist = ({ playlist }) => {
-  return <div>{playlist.name}</div>;
+  const color = getBGColor(playlist.id);
+
+  return (
+    <GradientLayout color={color} roundImage={false} title={playlist.name} subtitle="playlist" description={`${playlist.songs.length} songs`} image={`https://picsum.photos/400?random=${playlist.id}`}>
+      <SongsTable songs={undefined} />
+    </GradientLayout>
+  );
 };
 
 export const getServerSideProps = async ({ query, req }) => {
