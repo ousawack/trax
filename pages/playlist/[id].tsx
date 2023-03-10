ff1c5e17ee953fd2bb9984c54e64572b89fd6617
ousama <ousawack@gmail.com>
2023-03-10 19:20:10 +0100

    auth error handling

--- pages/playlist/[id].tsx
+++ pages/playlist/[id].tsx
@@ -37,11 +37,21 @@ const Playlist = ({ playlist }) => {
 };
 
 export const getServerSideProps = async ({ query, req }) => {
-  const { id } = validateToken(req.cookies.TRAX_ACCESS_TOKEN);
+  let user
+  try {
+    user = validateToken(req.cookie.TRAX_ACCESS_TOKEN);
+  } catch (e) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/singin"
+      }
+    }
+  }
   const [playlist] = await prisma.playlist.findMany({
     where: {
       id: +query.id,
-      userId: id,
+      userId: user.id,
     },
     include: {
       songs: {
