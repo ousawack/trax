9761e38cf6ce404d0457cc1bf05c68f8e41e0517
ousama <ousawack@gmail.com>
2023-03-14 17:29:20 +0100

    end song & repeat handlers

--- components/player.tsx
+++ components/player.tsx
@@ -26,7 +26,7 @@ import { formatTime } from "../lib/formatters";
 
 export const Player = ({ songs, activeSong }) => {
   const [playing, setPlaying] = useState(true);
-  const [index, setIndex] = useState(0);
+  const [index, setIndex] = useState(songs.findIndex((s) => s.id === activeSong.id));
   const [seek, setSeek] = useState(0.0);
   const [isSeeking, setIsSeeking] = useState(false);
   const [repeat, setRepeat] = useState(false);
@@ -34,6 +34,9 @@ export const Player = ({ songs, activeSong }) => {
   const [duration, setDuration] = useState(0.0);
   
   const soundRef = useRef(null);
+  const repeatRef = useRef(repeat)
+
+  const setActiveSong = useStoreActions((state : any) => state.changeActiveSong)
 
   useEffect(() => {
     let timerId
@@ -47,6 +50,14 @@ export const Player = ({ songs, activeSong }) => {
     }
     cancelAnimationFrame(timerId)
   }, [playing, isSeeking])
+
+  useEffect(() => {
+    setActiveSong(songs[index])
+  }, [index])
+
+  useEffect(() => {
+    repeatRef.current = repeat
+  }, [repeat])
 
   const setPlayState = (value) => {
     setPlaying(value);
@@ -81,7 +92,7 @@ export const Player = ({ songs, activeSong }) => {
   };
 
   const onEnd = () => {
-    if (repeat) {
+    if (repeatRef.current) {
       setSeek(0);
       soundRef.current.seek(0);
     } else {
@@ -178,7 +189,7 @@ export const Player = ({ songs, activeSong }) => {
             <RangeSlider
               aria-label={["min", "max"]}
               step={0.1}
-              maxW={duration ? duration.toFixed(2) : 0}
+              max={duration ? duration.toFixed(2) : 0}
               onChange={onSeek}
               value={[seek]}
               id="player-change"
